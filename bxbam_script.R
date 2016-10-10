67310e7987d8af1baa367a2b7df221f642738033
Evan Biederstedt <evan.biederstedt@gmail.com>
2016-10-10 02:42:46 -0400

    added R scipt

---
+++ bxbam_script.R
@@ -0,0 +1,32 @@
+######
+#%install.packages("rPython")
+#%install.packages("h5")
+
+library(rPython)
+                        
+
+# Load python script
+python.load("bxbam.py")   # i.e. this script
+
+bxbam_store <-h5file(bxbam)
+
+bam_path <- readline("Enter bam path")
+bxbam_path <- readline("Enter desired location/path for bxbam")
+           
+column1 <- "QNAME"
+column2 <- readline("Enter field to index, e.g. BX")   # default option?
+                        
+get_bmates <- function(reads, "BX" ) {
+    pand_func_bmate <- python.exec(bxbamstore.select("BX"))
+    python.call("pand_func_bmate", param1)
+}
+
+get_qmates <- function(bxbamstore, param1, param2){
+    pand_func_qmate <- python.exec(bxbamstore.select("QNAME")
+    python.call("pand_func_qmate", param1)
+}
+
+get_field <- function(reads, param1) {
+    pand_func <- python.exec(bxbamstore.select(param1))
+    python.call("pand_func", param1)
+}
