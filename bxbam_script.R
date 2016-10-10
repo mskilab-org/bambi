1797d8d094ab48e9b19223910fef0123de74f156
Evan Biederstedt <evan.biederstedt@gmail.com>
2016-10-10 18:57:34 -0400

    updated R script

--- bxbam_script.R
+++ bxbam_script.R
@@ -6,7 +6,7 @@ library(rPython)
                         
 
 # Load python script
-python.load("bxbam.py")   # i.e. this script
+python.load("bxbam.py")  
 
 bxbam_store <-h5file(bxbam)
 
@@ -17,16 +17,18 @@ column1 <- "QNAME"
 column2 <- readline("Enter field to index, e.g. BX")   # default option?
                         
 get_bmates <- function(reads, "BX" ) {
-    pand_func_bmate <- python.exec(bxbamstore.select("BX"))
+    pand_func_bmate <- python.exec(bxbam_store.select("BX"))
     python.call("pand_func_bmate", param1)
 }
 
 get_qmates <- function(bxbamstore, param1, param2){
-    pand_func_qmate <- python.exec(bxbamstore.select("QNAME")
+    pand_func_qmate <- python.exec(bxbam_store.select("QNAME")
     python.call("pand_func_qmate", param1)
 }
 
-get_field <- function(reads, param1) {
-    pand_func <- python.exec(bxbamstore.select(param1))
-    python.call("pand_func", param1)
-}
+#
+# get_field <- function(reads, param1) {
+#    pand_func <- python.exec(bxbamstore.select(param1))
+#    python.call("pand_func", param1)
+# }
+#
