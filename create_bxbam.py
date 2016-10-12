b80c484d3665411cf16dcbde820afc7080221696
Evan Biederstedt <evan.biederstedt@gmail.com>
2016-10-12 16:11:27 -0400

    updated create_bxbam.py

--- create_bxbam.py
+++ create_bxbam.py
@@ -15,15 +15,32 @@ args = parser.parse_args()
 bam_path = args.input
 bxbam_name = args.output
 fields = args.index_fields  # at the moment, users are limited by five fields which they can index
-fields = fields.split(',')
-field1 = index_fields[0]
-field2 = index_fields[1]
-if len(fields) == 3:
+fields = fields[0].split(',')
+if  len(fields) == 1:
+    field3 = index_fields[0]
+elif len(fields) == 2:
+    field3 = index_fields[1]
+elif len(fields) == 3:
     field3 = index_fields[2]
 elif len(fields) == 4:
     field4 = index_fields[3]
 elif len(fields) == 5:
     field5 = index_fields[4]
+
+print("input bam entered: "+ bam_path)
+print("bxbam file output: "+ bxbam_name)
+if len(fields) == 0:
+    print("no indexing")
+elif len(fields) == 1:
+    print("indexing on: "+ fields1)
+elif len(fields) == 2:
+    print("indexing on: "+ fields1 + ", " + fields2)
+elif len(fields) == 3:
+    print("indexing on: "+ fields1 + ", " + fields2 + ", " + fields3)
+elif len(fields) == 4:
+    print("indexing on: "+ fields1 + ", " + fields2 + ", " + fields3 + ", " + fields4)
+elif len(fields) == 5:
+    print("indexing on: "+ fields1 + ", " + fields2 + ", " + fields3 + ", " + fields4 + ", " + fields2)
 
 
 # chose fields to index
@@ -31,7 +48,9 @@ elif len(fields) == 5:
 # col2 = "BX"
 # columns_to_index = [col1, col2]   # list columns to index now, only
 
-if len(fields) == 2:
+if  len(fields) == 1:
+    columns_to_index = [field1]
+elif len(fields) == 2:
     columns_to_index = [field1, field2]
 elif len(fields) == 3:
     columns_to_index = [field1, field2, field3]
@@ -106,21 +125,23 @@ for i, line in enumerate(l.decode(errors='ignore') for l in task.stdout):  # dec
     merged_dict = newline.copy() # first dictionary...best method for Python pre-3.5
     merged_dict.update(newline1) # ...merged with second dictionary
     bxbam["QNAME"] = merged_dict["QNAME"] if merged_dict["QNAME"] != "" else "NaN"
-    bxbam["FLAG"]  = merged_dict["FLAG"] if merged_dict["FLAG"] != "" else "NaN"
-    bxbam["RNAME"]  = merged_dict["RNAME"] if merged_dict["RNAME"] != "" else "NaN"
-    bxbam["POS"]  = merged_dict["POS"] if merged_dict["POS"] != "" else "NaN"
-    bxbam["MAPQ"]  = merged_dict["MAPQ"] if merged_dict["MAPQ"] != "" else "NaN"
-    bxbam["CIGAR"]  = merged_dict["CIGAR"] if merged_dict["CIGAR"] != "" else "NaN"
-    bxbam["RNEXT"]  = merged_dict["RNEXT"] if merged_dict["RNEXT"] != "" else "NaN"
-    bxbam["PNEXT"]  = merged_dict["PNEXT"] if merged_dict["PNEXT"] != "" else "NaN"
-    bxbam["TLEN"]  = merged_dict["TLEN"] if merged_dict["TLEN"] != "" else "NaN"
-    bxbam["SEG"]  = merged_dict["SEQ"] if merged_dict["SEQ"] != "" else "NaN"
-    bxbam["QUAL"]  = merged_dict["QUAL"] if merged_dict["QUAL"] != "" else "NaN"
-    bxbam["BX"]  = merged_dict["BX"] if merged_dict["BX"] != "" else "NaN"
+    bxbam["FLAG"]  = merged_dict["FLAG"]  if merged_dict["FLAG"] != ""  else "NaN"
+    bxbam["RNAME"] = merged_dict["RNAME"] if merged_dict["RNAME"] != "" else "NaN"
+    bxbam["POS"]   = merged_dict["POS"]   if merged_dict["POS"] != ""   else "NaN"
+    bxbam["MAPQ"]  = merged_dict["MAPQ"]  if merged_dict["MAPQ"] != ""  else "NaN"
+    bxbam["CIGAR"] = merged_dict["CIGAR"] if merged_dict["CIGAR"] != "" else "NaN"
+    bxbam["RNEXT"] = merged_dict["RNEXT"] if merged_dict["RNEXT"] != "" else "NaN"
+    bxbam["PNEXT"] = merged_dict["PNEXT"] if merged_dict["PNEXT"] != "" else "NaN"
+    bxbam["TLEN"]  = merged_dict["TLEN"]  if merged_dict["TLEN"] != ""  else "NaN"
+    bxbam["SEG"]   = merged_dict["SEQ"]   if merged_dict["SEQ"] != ""   else "NaN"
+    bxbam["QUAL"]  = merged_dict["QUAL"]  if merged_dict["QUAL"] != ""  else "NaN"
+    bxbam["BX"]    = merged_dict["BX"]    if merged_dict["BX"] != ""    else "NaN"
     # This injects the Record values
     bxbamrow.append()
     # creates indices
-    if len(fields) == 2:
+    if  len(fields) == 1:
+        table.cols.field1.create_index()
+    elif len(fields) == 2:
         table.cols.field1.create_index()
         table.cols.field2.create_index()
     elif len(fields) == 3:
