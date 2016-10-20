496257ee7a9ec477a988dac097229d4402229a27
Evan Biederstedt <evan.biederstedt@gmail.com>
2016-10-20 13:56:34 -0400

    updated R functions

--- bxbam_script.R
+++ bxbam_script.R
@@ -1,22 +1,24 @@
-######
-#%install.packages("rPython")
-#%install.packages("h5")    necessary???
+#' @import rPython
+#' @import data.table
+#' @import python.exec("import pandas as pd")
+#' @import python.exec("import numpy as np")
+#' @import python.exec("import tables")
 
-library(rPython)
-library(data.table)
-
-
-# Load python script
-python.load("bxbam_functions.py")
-
-python.exec("import pandas as pd")
-python.exec("import numpy as np")
-python.exec("import tables")
-
+#' @export hdf_key
+#' key defined upon creation of bxBam via 'create_bxbam.py'
+#' If users changed this value in 'create_bxbam.py', override here
 hdf_key = "bam_fields"
+python.assign("hdf_key", hdf_key)
 
-bxbam_file <- readline("Prompt: Enter bxbam file name")
-
+#' @name load_bxbam
+#' @title load_bxbam
+#' @description
+#'
+#' Load the bxbam object (HDF5 format) to be queried via pandas HDFStore() object (PyTables)
+#' The default key ('HDF5 group') created using the Python script `create_bxbam.py` is hdf_key = "bam_fields"
+#' @param bxbam_file Input HDF5 file to be read, created by script 'create_bxbam.py'
+#' @return Reads in bxbam objects
+#' @export
 load_bxbam <- function(bxbam_file){     # e.g. bxbam_file is "file1.h5"
     python.assign("bxbam_file", bxbam_file)
     python.exec("store = pd.HDFStore(bxbam_file)")
@@ -24,27 +26,43 @@ load_bxbam <- function(bxbam_file){     # e.g. bxbam_file is "file1.h5"
     python.get("store")
 }
 
-#
-# Users input R vector of bmates
-# bmates_vec <- c("bx1", "bx2", "bx3", "bx4")
-#
+
+#' @name get_bmates
+#' @title get_bmates
+#' @description
+#'
+#' Reads R vector of BX strings, outputs GRanges or data.table
+#'
+#' Function used to output all corresponding bam fields and GRanges via data.table
+#' @param reads R vector of BX strings, e.g. bmates_vec <- c("bx1", "bx2", "bx3", "bx4")
+#' @return Corresponding fields in a bam file in data.table, GRanges, or GRangesList
+#' @export
 
 get_bmates <- function(reads){
     python.assign("reads", reads)
     pand_func_bmate <- python.exec("df = store.select(hdf_key, where='BX in @reads'")
     df <- python.get("df")
     df <- data.table(df)
+    return(df)
 }
 
-#
-# users write/read
-
+#' @name get_qmates
+#' @title get_qmates
+#' @description
+#'
+#' Reads R vector of QNAME strings, outputs GRanges or data.table
+#'
+#' Function used to output all corresponding bam fields and GRanges via data.table
+#' @param reads R vector of BX strings, e.g. bmates_vec <- c("bx1", "bx2", "bx3", "bx4")
+#' @return Corresponding fields in a bam file in data.table, GRanges, or GRangesList
+#' @export
 
 get_qmates <- function(reads){
     python.assign("reads", reads)
     pand_func_bmate <- python.exec("df = store.select(hdf_key, where='QNAME in @reads'")
     df <- python.get("df")
     df <- data.table(df)
+    return(df)
 }
 
 
