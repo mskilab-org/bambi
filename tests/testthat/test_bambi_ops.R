3c8180f6ef7266a0765c0f7f6cce339eec4361dd
Evan Biederstedt <evan.biederstedt@gmail.com>
2018-05-29 15:12:21 -0400

    revised tests with data.table

--- tests/testthat/test_bambi_ops.R
+++ tests/testthat/test_bambi_ops.R
@@ -133,7 +133,8 @@ test_that('bambi test method grab_cb()', {
     ## expect_match(foocb$grab_cb('GTAGTCATCTGGGCCA-1'))$cigar, "98M")
     expect_equal(length(foocb$grab_cb('GTAGTCATCTGGGCCA-1')), 1)
     expect_equal(length(foocb$grab_cb('GTAGTCATCTGGGCCA-1', verbose=TRUE)), 1)
-    expect_equal(length(foocb$grab_cb('GTAGTCATCTGGGCCA-1', data.table=TRUE)), 25)
+    expect_equal(dim(foocb$grab_cb('GTAGTCATCTGGGCCA-1', data.table=TRUE))[1], 1)
+    expect_equal(dim(foocb$grab_cb('GTAGTCATCTGGGCCA-1', data.table=TRUE))[2], 25)
     ##
     ## if ((is.null(barcodes)) & (is.null(query))){
     expect_equal(foocb$grab_cb(), GRanges())
@@ -201,7 +202,8 @@ test_that('bambi test method grab_ub()', {
     ## expect_match(as.data.frame(fooub$grab_ub('ATACAAGCGG'))$cigar, "98M")
     expect_equal(length(fooub$grab_ub('ATACAAGCGG')), 1)
     expect_equal(length(fooub$grab_ub('ATACAAGCGG', verbose=TRUE)), 1)
-    expect_equal(length(fooub$grab_ub('ATACAAGCGG', verbose=TRUE, data.table=TRUE)), 25)
+    expect_equal(dim(fooub$grab_ub('ATACAAGCGG', verbose=TRUE, data.table=TRUE))[1], 1)
+    expect_equal(dim(fooub$grab_ub('ATACAAGCGG', verbose=TRUE, data.table=TRUE))[2], 25)
     ##
     ## if ((is.null(barcodes)) & (is.null(query))){
     expect_equal(fooub$grab_ub(), GRanges())
