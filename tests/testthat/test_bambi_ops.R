718bd4aefcc3ebbfba7b01a753d6b775ddd3859d
Evan Biederstedt <evan.biederstedt@gmail.com>
2018-05-30 14:23:09 -0400

    added BAMs for UCSC/Ensembl queries for grab_ub()

--- tests/testthat/test_bambi_ops.R
+++ tests/testthat/test_bambi_ops.R
@@ -357,6 +357,11 @@ test_that('bambi test method grab_ub()', {
     expect_equal(fooub$grab_ub(query=GRanges("chr5:1053000-1253655")), NA)
     expect_equal(fooub$grab_ub(query=GRanges('chr5:10-15'), verbose=TRUE, data.table=TRUE), NA)
     expect_equal(fooub$grab_ub(query=GRanges("chr5:1053000-1253655", data.table=TRUE)), NA)
+    fooub_ucsc = bambi$new(bam_file = example_UB_bam_UCSC)
+    expect_equal(fooub_ucsc$grab_ub(query=GRanges('5:10-15'), verbose=TRUE), NA)
+    expect_equal(fooub_ucsc$grab_ub(query=GRanges("5:1053000-1253655")), NA)
+    expect_equal(fooub_ucsc$grab_ub(query=GRanges('5:10-15'), verbose=TRUE, data.table=TRUE), NA)
+    expect_equal(fooub_ucsc$grab_ub(query=GRanges("5:1053000-1253655", data.table=TRUE)), NA)
     ##
     expect_equal(length(fooub$grab_ub(query=GRanges("chr19:1440000-1440500"), verbose=TRUE)), 2)
     ####expect_equal(length(fooub$grab_ub(query=GRanges("chr19:1440000-1440500"), verbose=TRUE, data.table=TRUE)), 2)
